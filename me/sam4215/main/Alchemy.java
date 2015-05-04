d9ae4cfa78ac6662541260942f0c723d35598809
sam4215 <sam4215@users.noreply.github.com>
2015-05-04 13:19:49 -0500

    Let's get started!

---
+++ me/sam4215/main/Alchemy.java
@@ -0,0 +1,12 @@
+public class Alchemy {
+  private String name = "Alchemy"
+  private String creator = "Sam"
+  Board board = new Board();
+  public static void main(String[] args) {
+    if(args.contains("Forty-Two")) {
+      //Do stuff
+    }
+    board.start();
+  }
+}
+    
