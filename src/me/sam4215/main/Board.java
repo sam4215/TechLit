9a8dfb4cf91f7252de72fa355675c0f53bdc2de3
sam4215 <sam4215@users.noreply.github.com>
2015-05-06 13:01:16 -0500

    Update Board.java

--- src/me/sam4215/main/Board.java
+++ src/me/sam4215/main/Board.java
@@ -1,32 +1,39 @@
 public class Board {
   public int space;
-  public String[] cards = {"Everthing seems quiet, that's strange....", 
+  public String[] cards = {
+  "Everthing seems quiet, that's strange....", 
   "Time to take a rest....", 
   "Fight the odd battle.", 
   "This, I daresay, is quite strange...."
   }
-  public String[] special = {"Ooh, a dragon!", "Hide!", "Ooh boy."}
+  public String[] special = {
+  "Ooh, a dragon!", 
+  "Hide!", 
+  "Ooh boy.", 
+  "Well, this is not good."
+  }
   public void start() {
     space = 1;
+    playGame();
   }
-  public void nextTurn() {
-    if(space >= 25) {
+  public void playGame() {
+    while(space <= 25) {
+      int s = Math.random();
+      if(s > 5 || s < 0) {
+        for(String str : cards) {
+          System.out.println(str);
+        }
+        space++;
+      }
+      else {
+        for(String str : special) {
+          System.out.println(str);
+        }
+        int nextSpace = space + Math.random();
+        nextSpace = space;
+     }
+    }
       System.out.println("You win!");
       System.exit(0);
-    }
-    int s = Math.random();
-    if(s > 5 || s < 0) {
-      for(String str : cards) {
-        System.out.println(str);
-      }
-      space++;
-    }
-    else {
-      for(String str : special) {
-        System.out.println(str);
-      }
-      int nextSpace = space + Math.random();
-      nextSpace = space;
-    }
   }
 }
