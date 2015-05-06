9f9cfbb24fa7b2ef2efd72b6a51661d23776cdcb
sam4215 <sam4215@users.noreply.github.com>
2015-05-06 13:31:51 -0500

    Update Alchemy.java

--- src/main/java/me/sam4215/alchemy/Alchemy.java
+++ src/main/java/me/sam4215/alchemy/Alchemy.java
@@ -3,10 +3,6 @@ public class Alchemy {
   private String creator = "Sam";
   Board board = new Board();
   public static void main(String[] args) {
-    if(args.contains("Forty-Two")) { //Sssshhhh....
-      System.out.println("You win!");
-      System.exit(0);
-    }
     board.start();
   }
 }
