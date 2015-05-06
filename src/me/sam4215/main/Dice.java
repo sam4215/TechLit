fe2bac7a810bbbe845a41b8d1ec2e787c48e5a91
sam4215 <sam4215@users.noreply.github.com>
2015-05-06 12:58:02 -0500

    Update Dice.java

--- src/me/sam4215/main/Dice.java
+++ src/me/sam4215/main/Dice.java
@@ -2,7 +2,7 @@ public class Dice {
   public int[] output = {1, 2, 3, 4, 5, 6}
   public void roll() {
     for(int i : output) {
-      System.out.println(i);
+      System.out.println("Dice roll output: " + i + "!");
     }
   }
 }
