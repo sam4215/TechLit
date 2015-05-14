7dd1a420789bf34bb13e69a59412a2a6872a8a2b
sam4215 <sam4215@users.noreply.github.com>
2015-05-14 13:26:11 -0500

    Pweeze?
    Signed-off-by: Sam (sam4215)

--- src/main/java/me/sam4215/alchemy/Dice.java
+++ src/main/java/me/sam4215/alchemy/Dice.java
@@ -4,7 +4,7 @@ public class Dice {
   public double[] output = {1, 2, 3, 4, 5, 6};
   public void roll() {
     for(double d : output) {
-      public double diceRoll == d;
+      double diceRoll = d;
       System.out.println("Dice roll output: " + d + "!");
     }
   }
