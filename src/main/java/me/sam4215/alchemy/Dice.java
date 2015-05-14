2c3a97036b5a6a15ade1d175dd3ba78ec9e84bd8
sam4215 <sam4215@users.noreply.github.com>
2015-05-14 13:10:47 -0500

    Update Dice.java

--- src/main/java/me/sam4215/alchemy/Dice.java
+++ src/main/java/me/sam4215/alchemy/Dice.java
@@ -1,9 +1,9 @@
 package me.sam4215.alchemy;
 
 public class Dice {
-  public int[] output = {1, 2, 3, 4, 5, 6};
+  public double[] output = {1, 2, 3, 4, 5, 6};
   public void roll() {
-    for(int i : output) {
+    for(double d : output) {
       System.out.println("Dice roll output: " + i + "!");
     }
   }
