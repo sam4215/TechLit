5e3f199cac4dcbd5a86fab062d0155f20e4c5921
Sam <sam4215@users.noreply.github.com>
2015-05-19 13:39:18 -0500

    PWEEZE

--- src/main/java/me/sam4215/alchemy/Dice.java
+++ src/main/java/me/sam4215/alchemy/Dice.java
@@ -2,7 +2,7 @@ package me.sam4215.alchemy;
 
 public class Dice {
   public double[] output = {1, 2, 3, 4, 5, 6};
-  public double diceRoll;
+  public double diceRoll();
   public void roll() {
     for(double d : output) {
       System.out.println("Dice roll output: " + d + "!");
